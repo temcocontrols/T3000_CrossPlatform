97de7159df41a35b8b1e3f0d72e2350435cd7680
Konstantin S. <havendv@gmail.com>
2017-03-23 12:44:13 +1000

    Added simple form project

---
+++ T3000/Forms/VariablesForm.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace T3000
+{
+    partial class VariablesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.prgView = new System.Windows.Forms.DataGridView();
+            this.addButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.openButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.currentLabel = new System.Windows.Forms.Label();
+            this.currentFileLabel = new System.Windows.Forms.Label();
+            this.N = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Label = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Description = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.IsManual = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.prgView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // prgView
+            // 
+            this.prgView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.prgView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.N,
+            this.Label,
+            this.Description,
+            this.IsManual});
+            this.prgView.Location = new System.Drawing.Point(12, 12);
+            this.prgView.Name = "prgView";
+            this.prgView.Size = new System.Drawing.Size(627, 449);
+            this.prgView.TabIndex = 0;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(645, 12);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(145, 32);
+            this.addButton.TabIndex = 1;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(645, 50);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(145, 32);
+            this.deleteButton.TabIndex = 2;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(648, 391);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(145, 32);
+            this.openButton.TabIndex = 3;
+            this.openButton.Text = "Open .PRG";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(648, 429);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(145, 32);
+            this.saveButton.TabIndex = 4;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            // 
+            // currentLabel
+            // 
+            this.currentLabel.AutoSize = true;
+            this.currentLabel.Location = new System.Drawing.Point(12, 464);
+            this.currentLabel.Name = "currentLabel";
+            this.currentLabel.Size = new System.Drawing.Size(44, 13);
+            this.currentLabel.TabIndex = 5;
+            this.currentLabel.Text = "Current:";
+            // 
+            // currentFileLabel
+            // 
+            this.currentFileLabel.AutoSize = true;
+            this.currentFileLabel.Location = new System.Drawing.Point(53, 464);
+            this.currentFileLabel.Name = "currentFileLabel";
+            this.currentFileLabel.Size = new System.Drawing.Size(31, 13);
+            this.currentFileLabel.TabIndex = 6;
+            this.currentFileLabel.Text = "none";
+            // 
+            // N
+            // 
+            this.N.HeaderText = "N";
+            this.N.Name = "N";
+            this.N.ReadOnly = true;
+            // 
+            // Label
+            // 
+            this.Label.HeaderText = "Label";
+            this.Label.Name = "Label";
+            // 
+            // Description
+            // 
+            this.Description.HeaderText = "Description";
+            this.Description.Name = "Description";
+            // 
+            // IsManual
+            // 
+            this.IsManual.HeaderText = "IsManual";
+            this.IsManual.Name = "IsManual";
+            // 
+            // VariablesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(801, 485);
+            this.Controls.Add(this.currentFileLabel);
+            this.Controls.Add(this.currentLabel);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.openButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.prgView);
+            this.Name = "VariablesForm";
+            this.Text = "Variables";
+            ((System.ComponentModel.ISupportInitialize)(this.prgView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView prgView;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Label currentLabel;
+        private System.Windows.Forms.Label currentFileLabel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn N;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Label;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Description;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn IsManual;
+    }
+}
+
---
+++ T3000/Forms/VariablesForm.cs
@@ -0,0 +1,63 @@
+﻿namespace T3000
+{
+    using System;
+    using System.Windows.Forms;
+    using PRGReaderLibrary;
+
+    public partial class VariablesForm : Form
+    {
+        private PRG prg { get; set; }
+
+        public VariablesForm()
+        {
+            InitializeComponent();
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = ".PRG files (*.prg)|*.prg|All files (*.*)|*.*";
+            dialog.Title = "Please select an .PRG file";
+            if (dialog.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+
+            try
+            {
+                var path = dialog.FileName;
+                LoadPrg(path);
+                ShowCurrentFile(path);
+            }
+            catch (Exception exception)
+            {
+                ShowException(exception);
+            }
+        }
+
+        private void LoadPrg(string path)
+        {
+            prg = PRG.Load(path);
+            for (var i = 0; i < prg.Vars.Count; ++i)
+            {
+                var variable = prg.Vars[i];
+                prgView.Rows.Add(new object[]
+                {
+                    i, variable.Description, variable.Label, variable.IsManual
+                });
+            }
+        }
+
+        private void ShowException(Exception exception)
+        {
+            currentLabel.Text = "Exception:";
+            currentFileLabel.Text = exception.Message;
+        }
+
+        private void ShowCurrentFile(string path)
+        {
+            currentLabel.Text = "Current:";
+            currentFileLabel.Text = path;
+        }
+    }
+}
