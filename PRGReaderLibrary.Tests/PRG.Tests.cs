3cbc26b17c7e40e594f91186d5fdc26cdaf131a8
Konstantin S. <havendv@gmail.com>
2017-03-28 03:48:46 +1000

    Fixed TestVariables test. BaseTests to BaseTests method.

--- PRGReaderLibrary.Tests/PRG.Tests.cs
+++ PRGReaderLibrary.Tests/PRG.Tests.cs
@@ -19,7 +19,7 @@
             //Test variables binary load/save compatible
             foreach (var variable in prg.Variables)
             {
-                //if (variable.Units == UnitsEnum.degC)
+                //if (variable.Units == UnitsEnum.Time)
                 {
                     //variable.Value = variable.Value;
                     //variable.ValueString = variable.ValueString;
@@ -33,7 +33,7 @@
             }
 
             prg.Save(temp);
-            FileAssert.AreEqual(originalFile, temp);
+            FileAssert.AreEqual(originalFile, temp, name);
 
             if (prg.Variables.Count > 0)
             {
@@ -76,100 +76,64 @@
         [Test]
         public void Prg_Asy1()
         {
+        }
+
+        [Test]
+        public void Prg_BaseTests()
+        {
+            //Current
+            BaseTest("BTUMeter.prg");
+
+            //Dos
             BaseTest("asy1.prg");
-        }
-
-        [Test]
-        public void Prg_Balsam2()
-        {
-            UnsupportedTest("balsam2.prg");
-        }
-
-        [Test]
-        public void Prg_90185()
-        {
-            UnsupportedTest("90185.prg");
-        }
-
-        [Test]
-        public void Prg_Panel1()
-        {
+            BaseTest("temco.prg");
+            BaseTest("panel2.prg");
+            BaseTest("panel11.prg");
+            BaseTest("testvariables.prg");
             BaseTest("panel1.prg");
+
+            //Unsupported
+            UnsupportedTest("balsam2.prg");
+            UnsupportedTest("90185.prg");
+            UnsupportedTest("SelfTestRev3.prg");
         }
 
         [Test]
         public void Prg_TestVariables()
         {
-            BaseTest("testvariables.prg");
             var prg = Prg.Load(TestUtilities.GetFullPathForTestFile("testvariables.prg"));
 
             PrintVariables(prg);
 
             var variable1 = prg.Variables[0];
-            Assert.AreEqual("FirstDescription    ", variable1.Description); //20 bytes  
-            Assert.AreEqual("FirstLabe", variable1.Label); //9 bytes
-            //Assert.AreEqual(5000, variable1.Value);
+            Assert.AreEqual("FirstDescription    ", variable1.Description);
+            Assert.AreEqual("FirstLabe", variable1.Label);
+            Assert.AreEqual(5.0F, variable1.Value);
             Assert.AreEqual(AutoManualEnum.Automatic, variable1.AutoManual);
-            //Assert.AreEqual(false, variable1.IsAnalog);
-            //Assert.AreEqual(false, variable1.IsControl);
+            Assert.AreEqual(DigitalAnalogEnum.Analog, variable1.DigitalAnalog);
+            Assert.AreEqual(ControlEnum.Off, variable1.Control);
             Assert.AreEqual(UnitsEnum.degC, variable1.Units);
 
             var variable2 = prg.Variables[1];
-            Assert.AreEqual("SecondDescription   ", variable2.Description); //20 bytes
-            Assert.AreEqual("SecondLab", variable2.Label); //9 bytes
-            //Assert.AreEqual(true, variable2.Value);
+            Assert.AreEqual("SecondDescription   ", variable2.Description);
+            Assert.AreEqual("SecondLab", variable2.Label);
+            //Assert.AreEqual(false, variable2.Value);
             Assert.AreEqual(AutoManualEnum.Manual, variable2.AutoManual);
-            //Assert.AreEqual(false, variable2.IsAnalog);
-            //Assert.AreEqual(false, variable2.IsControl);
+            Assert.AreEqual(DigitalAnalogEnum.Digital, variable2.DigitalAnalog);
+            Assert.AreEqual(ControlEnum.Off, variable2.Control);
             Assert.AreEqual(UnitsEnum.OffOn, variable2.Units);
 
             var variable3 = prg.Variables[2];
-            Assert.AreEqual("ThirdDescription    ", variable3.Description); //20 bytes
-            Assert.AreEqual("ThirdLabe", variable3.Label); //9 bytes
+            Assert.AreEqual("ThirdDescription    ", variable3.Description);
+            Assert.AreEqual("ThirdLabe", variable3.Label);
 
-            //var test = ((int)variable3.Value / 256 / 256 / 256) % 256;
-            //var one = ((int) variable3.Value / 256 / 256) % 256;
-            //var two = ((int)variable3.Value / 256) % 256;
-            //var three = (int)variable3.Value % 256;
-            //var dateTime = new TimeSpan(one, two, three);
-            //Console.WriteLine($"{test}, {one}, {two}, {three}");
-            //Assert.AreEqual(false, variable3.Value);
+            Assert.AreEqual(new TimeSpan(0, 22, 22, 22, 0), variable3.Value);
             Assert.AreEqual(AutoManualEnum.Automatic, variable3.AutoManual);
-            //Assert.AreEqual(false, variable3.IsAnalog);
-            //Assert.AreEqual(false, variable3.IsControl);
+            Assert.AreEqual(DigitalAnalogEnum.Analog, variable3.DigitalAnalog);
+            Assert.AreEqual(ControlEnum.Off, variable3.Control);
             Assert.AreEqual(UnitsEnum.Time, variable3.Units);
 
             Console.WriteLine(prg.PropertiesText());
-        }
-
-        [Test]
-        public void Prg_Panel11()
-        {
-            BaseTest("panel11.prg");
-        }
-
-        [Test]
-        public void Prg_Panel2()
-        {
-            BaseTest("panel2.prg");
-        }
-
-        [Test]
-        public void Prg_Temco()
-        {
-            BaseTest("temco.prg");
-        }
-
-        [Test]
-        public void Prg_BTUMeter()
-        {
-            BaseTest("BTUMeter.prg");
-        }
-
-        [Test]
-        public void Prg_SelfTestRev3()
-        {
-            UnsupportedTest("SelfTestRev3.prg");
         }
     }
 }
