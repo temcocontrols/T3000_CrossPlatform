32b4e1a6e65e4aa1838e4d5e8fc9d8c8e65b2dae
Konstantin S. <havendv@gmail.com>
2017-04-08 01:37:10 +1000

    Updated DebugUtilities. Added toText option

--- PRGReaderLibrary/Utilities/DebugUtilities.cs
+++ PRGReaderLibrary/Utilities/DebugUtilities.cs
@@ -12,17 +12,26 @@
         /// <param name="bytes1"></param>
         /// <param name="bytes2"></param>
         /// <returns></returns>
-        public static string CompareBytes(byte[] bytes1, byte[] bytes2, int offset = 0, bool onlyDif = true)
+        public static string CompareBytes(byte[] bytes1, byte[] bytes2, int offset = 0, bool onlyDif = true, int length = 0, bool toText = true)
         {
             var text = string.Empty;
-            for (var i = offset; i < Math.Min(bytes1.Length, bytes2.Length); ++i)
+            var maxLength = Math.Min(bytes1.Length, bytes2.Length);
+            length = length == 0 ? maxLength : Math.Min(maxLength, offset + length);
+            for (var i = offset; i < length; ++i)
             {
-                if (onlyDif && bytes1[i] == bytes2[i])
+                var isEquals = bytes1[i] == bytes2[i];
+                if (onlyDif && isEquals)
                 {
                     continue;
                 }
 
-                text += $"{i}:\t{bytes1[i]}\t{bytes2[i]}{Environment.NewLine}";
+                var isTextByte1 = toText && char.IsLetter((char) bytes1[i]);
+                var isTextByte2 = toText && char.IsLetter((char) bytes2[i]);
+                text += $"{i}:\t" +
+                        $"{(isTextByte1 ? (char)bytes1[i] : (object)bytes1[i])}\t" +
+                        $"{(isTextByte2 ? (char)bytes2[i] : (object)bytes2[i])}\t" +
+                        $"{(isEquals && !onlyDif ? "" : "<---")}" +
+                        $"{Environment.NewLine}";
             }
 
             return text;
