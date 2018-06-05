989f5914647d58ed61b4c59985ed0c87fb9edd9f
Allan <amartinez@aimservices.tech>
2018-06-04 23:52:47 -0400

    Moving Editor to branch

--- VCCEditor/TestEditor/stdafx.cpp
+++
@@ -1,7 +0,0 @@
-// stdafx.cpp: archivo de código fuente que contiene sólo las inclusiones estándar
-// TestEditor.pch será el encabezado precompilado
-// stdafx.obj contiene la información de tipos precompilada
-
-#include "stdafx.h"
-
-
--- VCCEditor/TestEditor/stdafx.h
+++
@@ -1,6 +0,0 @@
-// stdafx.h: archivo de inclusión de los archivos de inclusión estándar del sistema
-// o archivos de inclusión específicos de un proyecto utilizados frecuentemente,
-// pero rara vez modificados
-#pragma once
-
-// TODO: mencionar aquí los encabezados adicionales que el programa necesita
